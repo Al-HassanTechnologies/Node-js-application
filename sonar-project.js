087753ea2ce099bd58294d729921e8e384947dfd
Al-HassanTechnologies <technologiestraininghyd@gmail.com>
2021-11-07 17:31:38 +0530

    Added the Node JS project

---
+++ sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.108.218.93:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsApp',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'Passw0rd',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
+
